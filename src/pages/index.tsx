ac7d545b9e98d6949765293b7bb76227d975c48a
Daniel Lopez <dlopez807@gmail.com>
2023-10-11 19:56:57 -0700

    add dates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { InferGetStaticPropsType } from 'next'
+import { format, eachDayOfInterval, isWednesday } from 'date-fns'
 
 import { Main } from '@bacondotbuild/ui'
 
@@ -6,17 +7,26 @@ import Layout from '@/components/layout'
 import type { Content } from '@/lib/fetchContent'
 import fetchContent from '@/lib/fetchContent'
 
+const startDate = new Date(2023, 8, 6)
+const today = new Date()
+const wednesdays = eachDayOfInterval({
+  start: startDate,
+  end: today,
+}).filter(date => isWednesday(date))
+
 export default function Home(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
+  const dates = wednesdays.slice(0, props?.picks.length ?? wednesdays.length)
   return (
     <Layout>
       <Main className='flex flex-col p-4'>
-        <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
+        <div className='flex flex-grow flex-col items-center space-y-4'>
           <ul className='space-y-4'>
-            {props?.picks.map(pick => (
-              <li key={pick} className='text-2xl'>
-                {pick}
+            {props?.picks.map((pick, index) => (
+              <li key={pick}>
+                {format(dates.at(-1 * (index + 1)) ?? startDate, 'M.d.yy')}
+                <div className='text-2xl'>{pick}</div>
               </li>
             ))}
           </ul>
